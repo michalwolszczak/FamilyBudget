8052cd42d2eeead4a4865f4c58232688c30d1622
Michał Wolszczak <mwolszczak13@wp.pl>
2022-12-20 21:59:53 +0100

    Add project files.

---
+++ Database/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() 
+        {
+
+        }
+    }
+}
